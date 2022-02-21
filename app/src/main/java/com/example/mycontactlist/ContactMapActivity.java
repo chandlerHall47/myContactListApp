2b36b8349c8293b33b9d94c2bae4c667efa1e085
chandlerHall47 <86384101+chandlerHall47@users.noreply.github.com>
2022-02-21 03:41:21 -0500

    added battery percentage and compass functionality to device

--- app/src/main/java/com/example/mycontactlist/ContactMapActivity.java
+++ app/src/main/java/com/example/mycontactlist/ContactMapActivity.java
@@ -6,10 +6,15 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -20,6 +25,7 @@ import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -50,6 +56,10 @@ public class ContactMapActivity extends AppCompatActivity implements OnMapReadyC
     LocationCallback locationCallback;
     ArrayList<Contact> contacts = new ArrayList<>();
     Contact currentContact = null;
+    SensorManager sensorManager;
+    Sensor accelerometer;
+    Sensor magnetometer;
+    TextView textDirection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,8 +94,21 @@ public class ContactMapActivity extends AppCompatActivity implements OnMapReadyC
 
         initListButton();
         initSettingsButton();
-//        initGetLocationButton();
        initMapTypeButtons();
+
+
+       sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+       accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+       magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+       if(accelerometer != null && magnetometer != null) {
+           sensorManager.registerListener(mySensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+           sensorManager.registerListener(mySensorEventListener, magnetometer, SensorManager.SENSOR_DELAY_FASTEST);
+
+       }else  {
+           Toast.makeText(this, "Sensors not found", Toast.LENGTH_LONG).show();
+       }
+       textDirection = (TextView) findViewById(R.id.textHeading);
 
     }
 
@@ -325,6 +348,43 @@ public class ContactMapActivity extends AppCompatActivity implements OnMapReadyC
 
 
    }
+
+   private SensorEventListener mySensorEventListener = new SensorEventListener() {
+       @Override
+       public void onAccuracyChanged(Sensor sensor, int accuracy) { }
+        float[] accelerometerValues;
+        float[] magneticValues;
+
+
+       @Override
+       public void onSensorChanged(SensorEvent event) {
+           if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+               accelerometerValues = event.values;
+           if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+               magneticValues = event.values;
+           if(accelerometerValues!= null && magneticValues != null) {
+               float R[] = new float[9];
+               float I[] = new float[9];
+
+               boolean success = SensorManager.getRotationMatrix(R,I, accelerometerValues, magneticValues);
+
+           if(success) {
+               float orientation[] = new float[3];
+               SensorManager.getOrientation(R, orientation);
+
+               float azimut = (float) Math.toDegrees(orientation[0]);
+               if(azimut < 0.0f) { azimut+=360.0f;}
+               String direction;
+               if(azimut >=315 || azimut < 45){direction = "N";}
+               else if (azimut >= 225 && azimut < 315) {direction = "W";}
+               else if(azimut >= 135 && azimut < 225){ direction = "S";}
+               else{ direction = "E";}
+               textDirection.setText(direction);
+
+           }
+           }
+       }
+   };
 
 
 
